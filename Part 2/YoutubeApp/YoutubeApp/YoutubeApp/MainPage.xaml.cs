416012ab3deeccd3df4599111bab7e4cbcf83b43
Mac Mittereder <macmitt2300@gmail.com>
2016-11-25 23:48:55 -0500

    Part 2
    Set up ListView and class to fill in parameters to populate list

---
+++ Part 2/YoutubeApp/YoutubeApp/YoutubeApp/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using YoutubeApp.Classes;
+
+namespace YoutubeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MainListView.ItemsSource = new List<ListViewTemplate>
+            {
+                new ListViewTemplate
+                {
+                    Name = "One",
+                    Description = "One",
+                    OrderNumber = 1
+                },
+                new ListViewTemplate
+                {
+                    Name = "Two",
+                    Description = "Two",
+                    OrderNumber = 2
+                },
+                new ListViewTemplate
+                {
+                    Name = "Three",
+                    Description = "Three",
+                    OrderNumber = 3
+                },
+                new ListViewTemplate
+                {
+                    Name = "Four",
+                    Description = "Four",
+                    OrderNumber = 4
+                }
+            };
+        }
+    }
+}
