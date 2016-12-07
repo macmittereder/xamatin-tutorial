7cfe8664c42b448657920d7289d82aa4371d393b
Mac Mittereder <macmitt2300@gmail.com>
2016-12-07 14:49:26 -0500

    Part 5
    Added Buttons Example

---
+++ Part 5/YoutubeApp/YoutubeApp/YoutubeApp/Classes/ListViewTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeApp.Classes
+{
+    public class ListViewTemplate
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int OrderNumber { get; set; }
+    }
+}
